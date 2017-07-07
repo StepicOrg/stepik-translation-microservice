ade80cf7a235215ebd2e5fa92ab2c83ea18b106d
Maxim Averin <amax0703@gmail.com>
2017-07-07 18:26:30 +0300

    add small improvments

--- api_controller/models.py
+++ api_controller/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-import environ
+from django.conf import settings
 
 class ApiController(models.Model):
-    api_key =
+    api_key = settings.STEPIK_API_KEY
     base_url = models.CharField()
     api_version = 0.1
 
