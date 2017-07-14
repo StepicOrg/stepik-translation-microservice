5f31b809e61292ff0959ce8879923ce148653392
Maxim Averin <amax0703@gmail.com>
2017-07-14 17:27:34 +0300

    made interface for accessing models

---
+++ translation/models/__init__.py
@@ -0,0 +1,2 @@
+from .translation_services import YandexTranslator
+from .translation import TranslatedLesson, TranslationStep
---
+++ translation/models/translation.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Translation(models.Model):
+    class Meta:
+        abstract = True
+
+    stepik_id = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    lang = models.CharField(max_length=10, blank=False)
+    text = models.TextField(blank=False)
+    service_name = models.CharField(max_length=40)
+
+
+class TranslatedLesson(models.Model):
+    stepik_id = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    service_name = models.CharField(max_length=40)
+
+
+class TranslationStep(Translation):
+    lesson = models.ForeignKey(TranslatedLesson, on_delete=models.CASCADE, related_name="steps")
