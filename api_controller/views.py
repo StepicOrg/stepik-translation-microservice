435b7d05650b80d89631579d3c6e9d6b2901cb77
Maxim Averin <amax0703@gmail.com>
2017-07-18 15:40:45 +0300

    add working views

--- api_controller/views.py
+++ api_controller/views.py
@@ -19,12 +19,16 @@ class Translation(APIView):
         if self.request.query_params.get("lang"):
             lang = self.request.query_params.get("lang")
 
+        print(lang, service_name, pk)
         data = api_controller.get_translation(obj_type, pk, service_name, lang)
 
         if data is None:
             return self.error_response(404)
         else:
-            serializer = TranslationStepSerializer(data, many=True)
+            if data.count() > 1:
+                serializer = TranslationStepSerializer(list(data), many=True)
+            else:
+                serializer = TranslationStepSerializer(data[0])
             return Response(serializer.data)
 
     def error_response(self, number_error):
