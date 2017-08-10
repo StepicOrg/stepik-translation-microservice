870b3654d11738b30c950e8626f64eec449ee2e9
Maxim Averin <amax0703@gmail.com>
2017-08-10 17:41:46 +0300

    Create course model

--- api_controller/urls.py
+++ api_controller/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
+
 from api_controller.views import TranslatedStepViewSet, TranslatedLessonViewSet, AvailableLanguagesViewSet, \
-    TranslationalRatioViewSet
+    TranslationalRatioViewSet, TranslatedCourseViewSet
 
 steps_detail = TranslatedStepViewSet.as_view({
     "get": "retrieve",
@@ -16,17 +17,23 @@ lesson_detail = TranslatedLessonViewSet.as_view({
     "put": "update"
 })
 
+course_detail = TranslatedCourseViewSet.as_view({
+    "get": "retrieve",
+    "post": "create"
+})
+
 lesson_list = TranslatedLessonViewSet.as_view({"get": "list"})
-
 languages_list = AvailableLanguagesViewSet.as_view({"get": "retrieve"})
-
 ratio_detail = TranslationalRatioViewSet.as_view({"get": "retrieve"})
+course_list = TranslatedCourseViewSet.as_view({"get": "list"})
 
 urlpatterns = [
     url(r'^api/translation/steps/(?P<pk>[0-9]+)$', steps_detail, name="step-datail"),
     url(r'^api/translation/steps/$', steps_list, name="step-list"),
     url(r'^api/translation/lessons/(?P<pk>[0-9]+)$', lesson_detail, name="lesson-detail"),
     url(r'^api/translation/lessons/', lesson_list, name="lesson-list"),
+    url(r'^api/translation/courses/(?P<pk>[0-9]+)$', course_detail, name="course-detail"),
+    url(r'^api/translation/courses/', course_list, name="course-list"),
     url(r'^api/translational_ratio/(?P<obj_type>[a-z]+)/(?P<pk>[0-9]+)$', ratio_detail, name="translational_ratio"),
     url(r'^api/available_languages/(?P<obj_type>[a-z]+)/(?P<pk>[0-9]+)$', languages_list, name="available_languages"),
 ]
